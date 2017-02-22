fcab375c0f0fec3582c7e1c8f00c9d9f6b663ac4
mavolio <meghan.avolio@gmail.com>
2017-02-22 10:11:05 -0500

    doing a full example with pplot data (figure 3)

---
+++ Worked example.R
@@ -0,0 +1,123 @@
+##worked example with pplots
+
+setwd("~/Dropbox/converge_diverge/datasets/Longform")
+
+library(tidyr)
+library(dplyr)
+library(ggplot2)
+library(vegan)
+library(gridExtra)
+
+dat<-read.csv("SpeciesRelativeAbundance_Dec2016.csv")
+
+##pplots
+pplots<-dat%>%
+  filter(project_name=="pplots",calendar_year==2002|calendar_year==2014)%>%
+  tbl_df()%>%
+  group_by(calendar_year, plot_id)%>%
+  mutate(rank=rank(-relcov, ties.method="average"),
+         treatment=factor(treatment, levels=c("N1P0","N1P1","N1P2", "N1P3","N2P0","N2P1","N2P2", "N2P3")))###need to do this b/c otherwise R will remember every treatment
+
+##step 1. do NMDS of pretreatment and last year of data
+pplots_wide<-dat%>%
+  filter(project_name=="pplots",calendar_year==2002|calendar_year==2014)%>%
+  select(treatment, calendar_year, plot_id, genus_species, relcov)%>%
+  spread(genus_species, relcov, fill=0)
+
+plots<-pplots_wide[,1:3]
+mds<-metaMDS(pplots_wide[,4:75], autotransform=FALSE, shrink=FALSE)
+mds
+
+adonis(pplots_wide[,4:75]~treatment, pplots_wide)
+
+#differences in dispersion?
+dist<-vegdist(pplots_wide[,4:75])
+betadisp<-betadisper(dist,pplots_wide$treatment,type="centroid")
+betadisp
+permutest(betadisp)
+
+scores <- data.frame(scores(mds, display="sites"))  # Extracts NMDS scores for year "i" #
+scores2<- cbind(plots, scores) # binds the NMDS scores of year i to all years previously run
+
+##facet_wrap
+toplot<-scores2%>%
+  filter(treatment=="N1P0"|treatment=="N2P0"|treatment=="N2P3")
+##plot NMDS
+ggplot(toplot, aes(x=NMDS1, y=NMDS2, color=as.factor(calendar_year)))+
+  geom_point(size=5)+
+  facet_wrap(~treatment)
+##plot RACS
+ractoplot<-pplots%>%
+  filter(treatment=="N1P0"|treatment=="N2P0"|treatment=="N2P3")
+ggplot(data=ractoplot, aes(x=rank, y=relcov))+
+  geom_point(aes(color=genus_species), size=3)+
+  geom_line(aes(group=plot_id), size=0.2)+
+  facet_wrap(~treatment+calendar_year, ncol=2)
+
+
+###figuring out cumulative curve
+average<-ractoplot%>%
+  group_by(calendar_year, treatment, genus_species)%>%
+  summarize(relcov=mean(relcov))%>%
+  mutate(sumabund=sum(relcov),
+         relcov2=relcov/sumabund)%>%
+  mutate(rank=rank(-relcov2, ties.method="average"),
+         maxrank=max(rank),
+         relrank=rank/maxrank)%>%
+  arrange(relrank)%>%
+  mutate(cumabund=cumsum(relcov2))
+
+ggplot(data=average, aes(x=relrank, y=cumabund))+
+  geom_point(size=3)+
+  geom_line(size=1, aes(color=as.factor(calendar_year), group=calendar_year))+
+  facet_wrap(~treatment, ncol=2)
+
+###cherry picked examples
+average_test<-pplots%>%
+  group_by(calendar_year, treatment, plot_id)%>%
+  mutate(rank=rank(-relcov, ties.method="average"),
+         maxrank=max(rank),
+         relrank=rank/maxrank)%>%
+  arrange(relrank)%>%
+  mutate(cumabund=cumsum(relcov))
+
+result <- average_test %>%
+  group_by(plot_id) %>%
+  do({
+    y <- unique(.$calendar_year)###assumption this is a length 2 list
+    df1 <- filter(., calendar_year==y[[1]])
+    df2 <- filter(., calendar_year==y[[2]])
+    sf1 <- stepfun(df1$relrank, c(0, df1$cumabund))
+    sf2 <- stepfun(df2$relrank, c(0, df2$cumabund))
+    r <- sort(unique(c(0, df1$relrank, df2$relrank)))
+    h <- abs(sf1(r) - sf2(r))
+    w <- c(diff(r), 0)
+    data.frame(
+     # Dmax=max(h),
+      Dstar=sum(w*h))
+  })
+
+trt<-pplots%>%
+  tbl_df()%>%
+  select(plot_id, treatment)%>%
+  unique()
+results_test<-merge(result, trt, by="plot_id")
+
+plot(results_test$Dmax, results_test$Dstar)
+
+boxplot(results_test$Dmax~results_test$treatment)
+boxplot(results_test$Dstar~results_test$treatment)
+anova(lm(Dstar~treatment, data=results_test))
+TukeyHSD(aov(Dstar~treatment, data=results_test))
+
+ggplot(data=average_test, aes(x=relrank, y=cumabund, group=interaction(calendar_year,plot_id)))+
+  geom_point(size=3, aes(color=as.factor(calendar_year)))+
+  geom_step(size=1, aes(color=as.factor(plot_id)))+##should we graph with geom_step?
+  facet_wrap(~treatment)
+
+
+
+
+
+
+
