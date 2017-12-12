f3f5fc979b888e2741b800f08cae54edfdc1bad9
Meghan Avolio <meghanavolio@Meghans-MB.local>
2017-12-12 13:48:21 -0500

    moved file

---
+++ Functions/calculate_SERGL.R
@@ -0,0 +1,69 @@
+##calculating SERGL
+calculate_SERGL <- function(df, replicate.var, species.var, abundance.var, time.var) {
+  rankdf <- add_ranks(df, replicate.var, species.var, abundance.var, time.var)
+  
+  # current year rankdf
+  df2 <- rankdf
+  
+  # previous year rank df
+  df1 <- rankdf
+  df1[[time.var]] <- df1[[time.var]] + 1
+  
+  # merge: .x is for previous time point, .y for current time point, time.var corresponds to current (i.e., .y)
+  df12 <- merge(df1, df2,  by=c(species.var,replicate.var, time.var), all=T)
+  df12<-subset(df12, df12[[paste(abundance.var, ".x", sep = "")]]!=0|df12[[paste(abundance.var, ".y", sep = "")]]!=0)
+  df12<-subset(df12, !is.na(df12[[paste(abundance.var, ".x", sep = "")]]) & !is.na(df12[[paste(abundance.var, ".y", sep = "")]]))
+  df12$splitvariable <- paste(df12[[replicate.var]], df12[[time.var]], sep="_") 
+  
+  # sort and apply turnover to all replicates
+  df12 <- df12[order(df12$splitvariable),]
+  X <- split(df12, df12$splitvariable)
+  
+
+  out <- lapply(X, FUN=aggfunc, "rank.x", "rank.y", paste(abundance.var, ".x", sep = ""),paste(abundance.var, ".y", sep = "")) 
+  ID <- unique(names(out))
+  out <- mapply(function(x, y) "[<-"(x, "splitvariable", value = y) ,
+                out, ID, SIMPLIFY = FALSE)
+  output <- do.call("rbind", out)  
+
+  outnames <- data.frame(do.call('rbind', strsplit(as.character(output$splitvariable),'_',fixed=TRUE)))
+  names(outnames) = c(replicate.var, time.var)
+  
+  output$splitvariable <- NULL
+  output <- cbind(outnames, output)
+  
+  return(output)
+}
+
+
+### PRIVATE FUNCTIONS ###
+## function for MRSc
+MRSc <- function(df, rank1, rank2){
+  mrcs <- mean(abs(df[[rank1]]-df[[rank2]]))/nrow(df)
+  return(mrcs)
+}
+
+
+## function for the richness and evenness differences, gains and losses, and returning a dataframe with those and the MRSc output
+aggfunc <- function(df, rank.var1, rank.var2, abundance.var1, abundance.var2){
+  #ricness and evenness differences
+  s_t1 <- S(df[[abundance.var1]])
+  e_t1 <- E_q(as.numeric(df[[abundance.var1]]))
+  s_t2 <- S(df[[abundance.var2]])
+  e_t2 <- E_q(as.numeric(df[[abundance.var2]]))
+  
+  sdiff <- abs(s_t1-s_t2)/nrow(df)
+  ediff <- abs(e_t1-e_t2)/nrow(df)
+  
+  #gains and losses
+  df$gain <- ifelse(df[[abundance.var1]]==0, 1, 0)
+  df$loss <- ifelse(df[[abundance.var2]]==0, 1, 0)
+  
+  gain <- sum(df$gain)/nrow(df)
+  loss <- sum(df$loss)/nrow(df)
+  
+  mrsc <- MRSc(df, rank.var1, rank.var2)
+  
+  metrics <- data.frame(S=sdiff, E=ediff, R=mrsc, G=gain, L=loss)
+  return(metrics)
+}
