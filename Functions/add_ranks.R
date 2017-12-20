43139ef9dc1e16d16ebf15497bd852daecebe082
Meghan <meghan.avolio@gmail.com>
2017-12-20 15:46:43 -0500

    adding null replicate

--- Functions/add_ranks.R
+++ Functions/add_ranks.R
@@ -16,28 +16,27 @@
 #'  \item{rank: }{A numeric column with the species rank; a rank of 1 indicates the species was most abundant in that time period. Species that are not present in that time period have the largest rank value.}
 #' }
 #' @export
-add_ranks <- function(df, replicate.var, species.var, abundance.var, time.var) {
-  ##SHOULD THE FIRST STEP TO BE ONLY SELECT THE RELEVANT COLUMNS?
-  df<-subset(df, select=c(time.var, species.var, abundance.var, replicate.var))
+add_ranks <- function(df, replicate.var=NULL, species.var, abundance.var, time.var) {
   
+  if(is.null(replicate.var)){
+    df<-subset(df, select=c(time.var, species.var, abundance.var))
   ##add ranks for present species
   rank_pres<-subset(df, df[[abundance.var]]!=0)
-  rank_pres$rep_time<-paste(rank_pres[[replicate.var]], rank_pres[[time.var]], sep="_")
-  rank_pres$rank<-ave(rank_pres[[abundance.var]], rank_pres$rep_time, FUN=function(x) rank(-x, ties.method = "average"))
-  rank_pres<-subset(rank_pres, select=-rep_time)
+   rank_pres$rank<-ave(rank_pres[[abundance.var]], rank_pres[[time.var]], FUN=function(x) rank(-x, ties.method = "average"))
+  rank_pres<-subset(rank_pres)
   
   #adding zeros
   
-  # sort and apply fill_zeros to all replicates
-  df <- df[order(df[[replicate.var]]),]
-  X <- split(df, df[replicate.var])
-  out <- lapply(X, FUN=fill_zeros, time.var, species.var, abundance.var)
-  ID <- unique(names(out))
-  out <- mapply(function(x, y) "[<-"(x, replicate.var, value = y) ,
-                out, ID, SIMPLIFY = FALSE)
-  allsp <- do.call("rbind", out)
+  # sort and fill zeros 
+  
+  df2<-subset(df, select=c(time.var,species.var,abundance.var))
+  wide<- reshape(df2, idvar=time.var, timevar=species.var, direction="wide")
+  wide[is.na(wide)] <- 0
+  
+  long<-reshape(wide, idvar=time.var, ids=time.var, time=names(wide), timevar=abundance.var, direction="long")
+  colnames(long)[3]<-abundance.var
+  allsp<-long
 
- 
   ###make zero abundant species have the rank S+1 (the size of the species pool plus 1)
   ##pull out zeros
   zeros <- subset(allsp, allsp[[abundance.var]]==0)
@@ -45,18 +44,59 @@ add_ranks <- function(df, replicate.var, species.var, abundance.var, time.var) {
   ##get species richness for each year
   ## Note to Meghan: This uses a function that I thought was only for community_structure
   ## Might make a separate file of shared diversity functions
-  myformula <- as.formula(paste(abundance.var, "~", replicate.var, "+", time.var))
+  myformula <- as.formula(paste(abundance.var, "~", time.var))
   SpR<-aggregate(myformula, FUN=S, data=allsp)
-  colnames(SpR)[3]<-"S"
+  colnames(SpR)[2]<-"S"
   
   ##merge together make zero abundances rank S+1
-  zero_rank<-merge(zeros, SpR, by=c(time.var,replicate.var))
+  zero_rank<-merge(zeros, SpR, by=c(time.var))
   zero_rank$rank<-zero_rank$S+1
   zero_rank<-subset(zero_rank, select=-S)
   
   ##combine all
   rank<-rbind(rank_pres, zero_rank)
   
+  }
+  
+  else {
+    df<-subset(df, select=c(time.var, species.var, abundance.var, replicate.var))
+    ##add ranks for present species
+    rank_pres<-subset(df, df[[abundance.var]]!=0)
+    rank_pres$rep_time<-paste(rank_pres[[replicate.var]], rank_pres[[time.var]], sep="_")
+    rank_pres$rank<-ave(rank_pres[[abundance.var]], rank_pres$rep_time, FUN=function(x) rank(-x, ties.method = "average"))
+    rank_pres<-subset(rank_pres, select=-rep_time)
+    
+    #adding zeros
+    
+    # sort and apply fill_zeros to all replicates
+    df <- df[order(df[[replicate.var]]),]
+    X <- split(df, df[replicate.var])
+    out <- lapply(X, FUN=fill_zeros, time.var, species.var, abundance.var)
+    ID <- unique(names(out))
+    out <- mapply(function(x, y) "[<-"(x, replicate.var, value = y) ,
+                  out, ID, SIMPLIFY = FALSE)
+    allsp <- do.call("rbind", out)
+    
+    
+    ###make zero abundant species have the rank S+1 (the size of the species pool plus 1)
+    ##pull out zeros
+    zeros <- subset(allsp, allsp[[abundance.var]]==0)
+    
+    ##get species richness for each year
+    ## Note to Meghan: This uses a function that I thought was only for community_structure
+    ## Might make a separate file of shared diversity functions
+    myformula <- as.formula(paste(abundance.var, "~", replicate.var, "+", time.var))
+    SpR<-aggregate(myformula, FUN=S, data=allsp)
+    colnames(SpR)[3]<-"S"
+    
+    ##merge together make zero abundances rank S+1
+    zero_rank<-merge(zeros, SpR, by=c(time.var,replicate.var))
+    zero_rank$rank<-zero_rank$S+1
+    zero_rank<-subset(zero_rank, select=-S)
+    
+    ##combine all
+    rank<-rbind(rank_pres, zero_rank)
+  }
   return(rank)
 }
 
@@ -84,3 +124,10 @@ fill_zeros <- function(df, time.var, species.var, abundance.var){
   return(long)
 }
 
+#1) function to calculate richness
+#' @x the vector of abundances of each species
+S<-function(x){
+  x1 <- x[x!=0 & !is.na(x)]
+  stopifnot(x1==as.numeric(x1))
+  length(x1)
+}
