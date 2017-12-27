8eabaa1ffd184b0195a736169f4581525b28d5ac
Meghan <meghan.avolio@gmail.com>
2017-12-27 16:29:01 -0500

    creating the function

--- Functions/curve_change.R
+++ Functions/curve_change.R
@@ -1,83 +1,108 @@
 ##calculating curve changes
 #' @title Curve Shape Changes
-#' @description this compares the areas of difference between two curves that are consequtive time steps for a plot.
+#' @description this compares the areas of difference between two curves that are consequtive time steps. It is optional to do this for replicates. For this to work, there must be more than 1 species in a plot at both time points, and when included, the replicate must also be measured at both time points.
 #' @param df A data frame containing time, species and abundance columns and an optional column of replicates
 #' @param time.var The name of the time column 
 #' @param species.var The name of the species column 
 #' @param abundance.var The name of the abundance column 
 #' @param replicate.var The name of the optional replicate column 
 
-curve_change <- function(df, time.var, replicate.var=NULL, ){
-  curvechange=data.frame(replicate=c(), time=c(), CC=c())#expeiment year is year of timestep2
+curve_change <- function(df, time.var, species.var, abundance.var, replicate.var=NULL){
   
-  ranks<-df%>%
-    filter(abundance!=0)%>%
-    group_by(time, replicate)%>%
-    mutate(rank=rank(-abundance, ties.method="average"),
-           maxrank=max(rank),
-           relrank=rank/maxrank)%>%
-    arrange(-abundance)%>%
-    mutate(cumabund=cumsum(abundance))%>%
-    ungroup()
+  #curvechange=data.frame(replicate=c(), time=c(), CC=c())#expeiment year is year of timestep2
+ 
+  if(is.null(replicate.var)){
   
-  timestep<-sort(unique(ranks$time))    
-  
-  for(i in 1:(length(timestep)-1)) {#minus 1 will keep me in year bounds NOT WORKING
-    subset_t1<-ranks%>%
-      filter(time==timestep[i])
+  df <- subset(df, select = c(time.var, species.var, abundance.var))
+  relrank <- subset(df, df[[abundance.var]]!=0)
+  relrank$rank <- ave(relrank[[abundance.var]], relrank[[time.var]], FUN = function(x) rank(-x, ties.method = "average"))
+  relrank$maxrank = ave(relrank$rank, relrank[[time.var]], FUN = function(x) max(x))
+  relrank$relrank = relrank$rank/relrank$maxrank
+  relrank <- relrank[order(relrank[[time.var]], -relrank[[abundance.var]]),]
+  relrank$cumabund <- ave(relrank[[abundance.var]], relrank[[time.var]], FUN = function(x) cumsum(x))
+ 
+  timestep<-sort(unique(relrank[[time.var]]))
+  cc_out<-data.frame()
+  for(i in 1:(length(timestep)-1)) {
+    subset_t1 <- relrank[relrank[[time.var]] == timestep[i],]
+    subset_t2 <- relrank[relrank[[time.var]] == timestep[i+1],]
+    subset_t12 <- rbind(subset_t1, subset_t2)
     
-    plots_t1<-subset_t1%>%
-      select(replicate)%>%
-      unique()
+    output <- curvechange(subset_t12)
     
-    subset_t2<-ranks%>%
-      filter(time==timestep[i+1])
-    
-    plots_t2<-subset_t2%>%
-      select(replicate)%>%
-      unique()
-    
-    plots_bothyrs<-merge(plots_t1, plots_t2, by="replicate")
-    #dataset of two years    
-    subset_t12<-rbind(subset_t1, subset_t2)
-    
-    ##dropping plots that were not measured both years
-    subset_t12_2<-merge(plots_bothyrs, subset_t12, by="replicate")
-    
-    #dropping plots with only 1 species in any of the two years    
-    drop<-subset_t12_2%>%
-      group_by(time, replicate)%>%
-      mutate(numplots=length(replicate))%>%
-      ungroup()%>%
-      group_by(replicate)%>%
-      mutate(min=min(numplots))%>%
-      select(replicate, min)%>%
-      unique()
-    
-    subset_t12_3<-merge(subset_t12_2, drop, by="replicate")%>%
-      filter(min!=1)%>%
-      ungroup()%>%
-      group_by(time, replicate)%>%
-      arrange(rank)%>%
-      ungroup()
-    
-    result <- subset_t12_3 %>%
-      group_by(replicate) %>%
-      do({
-        y <- unique(.$time)###assumption this is a length 2 list
-        df1 <- filter(., time==y[[1]])
-        df2 <- filter(., time==y[[2]])
-        sf1 <- stepfun(df1$relrank, c(0, df1$cumabund))
-        sf2 <- stepfun(df2$relrank, c(0, df2$cumabund))
-        r <- sort(unique(c(0, df1$relrank, df2$relrank)))
-        h <- abs(sf1(r) - sf2(r))
-        w <- c(diff(r), 0)
-        data.frame(CC=sum(w*h))#do has to output a dataframe
-      })
-    
-    d_output1=data.frame(replicate=result$replicate, time=timestep[i+1], CC=result$CC)#expeiment year is year of timestep2
-    
-    curvechange<-rbind(curvechange, d_output1)
+    cc_out <- rbind(cc_out, output)
+    }
   }
-  return(curvechange)
+  
+  else{
+    df <- subset(df, select = c(time.var, species.var, abundance.var, replicate.var))
+    relrank <- subset(df, df[[abundance.var]]!=0)
+    relrank$rep_time <- paste(relrank[[replicate.var]], relrank[[time.var]], sep="_")
+    relrank$rank <- ave(relrank[[abundance.var]], relrank$rep_time, FUN = function(x) rank(-x, ties.method = "average"))
+    relrank$maxrank = ave(relrank$rank, relrank$rep_time, FUN = function(x) max(x))
+    relrank$relrank = relrank$rank/relrank$maxrank
+    relrank <- relrank[order(relrank[[time.var]], relrank[[replicate.var]],-relrank[[abundance.var]]),]
+    relrank$cumabund <- ave(relrank[[abundance.var]], relrank$rep_time, FUN = function(x) cumsum(x))
+    
+    timestep<-sort(unique(relrank[[time.var]]))
+    cc_out<-data.frame()
+    
+    for(i in 1:(length(timestep)-1)) {
+      
+      subset_t1 <- relrank[relrank[[time.var]] == timestep[i],]
+      subset_t2 <- relrank[relrank[[time.var]] == timestep[i+1],]
+      subset_t12 <- rbind(subset_t1, subset_t2)
+      
+      X <- split(subset_t12, subset_t12[[replicate.var]])
+      out <- lapply(X, FUN=curvechange) 
+      ID <- unique(names(out))
+      out <- mapply(function(x, y) "[<-"(x, replicate.var, value = y) ,
+                    out, ID, SIMPLIFY = FALSE)
+      output <- do.call("rbind", out)
+
+      cc_out <- rbind(cc_out, output)
+    }
+  }
+    
+    return (cc_out)
 }
+    
+###private function
+    
+curvechange <- function(df){
+    
+    timestep2 <- unique(df[[time.var]])#assumes this is a length of 2
+
+    df1 <- df[df[[time.var]] == timestep2[1],]
+    df2 <- df[df[[time.var]] == timestep2[2],]
+    sf1 <- stepfun(df1$relrank, c(0, df1$cumabund))
+    sf2 <- stepfun(df2$relrank, c(0, df2$cumabund))
+    r <- sort(unique(c(0, df1$relrank, df2$relrank)))
+    h <- abs(sf1(r) - sf2(r))
+    w <- c(diff(r), 0)
+    CC=sum(w*h)
+    
+    
+    output=data.frame(timestep=timestep2[2], CurveChange=CC)#expeiment year is year of timestep2
+    colnames(output)[1]<-time.var
+    
+    return(output)
+    } 
+  
+    
+####these are going to give errors
+#1. plots that were not measured both year
+#2. plots with only 1 species in any 2 years.
+
+##dropping plots that were not measured both years
+subset_t12_2<-merge(plots_bothyrs, subset_t12, by="replicate")
+
+#dropping plots with only 1 species in any of the two years    
+drop<-subset_t12_2%>%
+  group_by(time, replicate)%>%
+  mutate(numplots=length(replicate))%>%
+  ungroup()%>%
+  group_by(replicate)%>%
+  mutate(min=min(numplots))%>%
+  select(replicate, min)%>%
+  unique()
