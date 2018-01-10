9d2d42cc5daab840ecc947022809a9c962cfea63
Meghan <meghan.avolio@gmail.com>
2018-01-10 13:33:06 -0500

    made time optional

--- Functions/community_diversity.R
+++ Functions/community_diversity.R
@@ -2,12 +2,12 @@
 #' @title Community structure
 #' @description 
 #' @param df A data frame containing time, species and abundance columns and an optional column of replicates
-#' @param time.var The name of the time column 
+#' @param time.var The name of the optional time column 
 #' @param species.var The name of the species column 
 #' @param abundance.var The name of the abundance column 
 #' @param replicate.var The name of the optional replicate column 
 
-community_diversity <- function(df,  time.var, abundance.var, replicate.var=NULL,  diversity = "Shannon") {
+community_diversity <- function(df,  time.var=NULL, abundance.var, replicate.var=NULL,  diversity = "Shannon") {
   if(is.null(replicate.var)){
     myformula <- as.formula(paste(abundance.var, "~", time.var))
     
@@ -19,11 +19,23 @@ community_diversity <- function(df,  time.var, abundance.var, replicate.var=NULL
     else{
       comdiv <- aggregate(myformula, data=df, FUN = function(x) diversity = Simpson(x))
       names(comdiv)[2] <- "Simpson"
-
-    }
+      }
   } 
   else {
-    
+    if(is.null(time.var)){
+      myformula <- as.formula(paste(abundance.var, "~", replicate.var))
+      
+      if(diversity == "Shannon"){
+        comdiv <- aggregate(myformula, data = df, FUN = function(x) diversity = Shannon(x))
+        names(comdiv)[2] <- "Shannon"
+        
+      }
+      else{
+        comdiv <- aggregate(myformula, data=df, FUN = function(x) diversity = Simpson(x))
+        names(comdiv)[2] <- "Simpson"
+      }
+    }
+    else{
     myformula <- as.formula(paste(abundance.var, "~", time.var, "+", replicate.var))
     
     if(diversity == "Shannon"){
@@ -33,6 +45,7 @@ community_diversity <- function(df,  time.var, abundance.var, replicate.var=NULL
     else{
       comdiv <- aggregate(myformula, data = df, FUN = function(x) diversity = Simpson(x))
       names(comdiv)[3] <- "Simpson"
+    }
     }
   }
   return(comdiv)
