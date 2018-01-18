0d10e8295c6169195c891c7b7a56daff9c8baef1
Lauren Hallett <hallett@uoregon.edu>
2018-01-18 14:34:44 -0800

    cosmetic changes to abundance_change

--- Functions/abundance_change.R
+++ Functions/abundance_change.R
@@ -19,10 +19,14 @@
 #' @export
 
 
-abundance_change <- function(df, time.var, species.var, abundance.var, replicate.var=NULL) {
-  if(is.null(replicate.var)){
+abundance_change <- function(df, time.var, 
+                             species.var, 
+                             abundance.var, 
+                             replicate.var = NULL) {
+  
+  if(is.null(replicate.var)) {
     
-    rankdf <- add_ranks_time(df, time.var, species.var, abundance.var, replicate.var=NULL)
+    rankdf <- add_ranks_time(df, time.var, species.var, abundance.var, replicate.var = NULL)
     
     time1 <- sort(unique(rankdf[[time.var]]))
     time2 <- c(time1[2:length(time1)], NA)
@@ -35,13 +39,13 @@ abundance_change <- function(df, time.var, species.var, abundance.var, replicate
     names(mytimes) = c(time.var, "dummytime")
     
     df1 <- merge(rankdf, mytimes)
-    names(df1)[1] <- 'time1'
-    names(df1)[[ncol(df1)]] <- time.var
+    names(df1)[1] = 'time1'
+    names(df1)[[ncol(df1)]] = time.var
     df1 <- subset(df1, !is.na(df1[[time.var]]))
     
     # merge: .x is for previous time point, .y for current time point, time.var corresponds to current (i.e., .y)
-    df12 <- merge(df1, df2,  by=c(species.var, time.var), all=T)
-    df12<-subset(df12, df12[[paste(abundance.var, ".x", sep = "")]]!=0|df12[[paste(abundance.var, ".y", sep = "")]]!=0)
+    df12 <- merge(df1, df2,  by = c(species.var, time.var), all = T)
+    df12<-subset(df12, df12[[paste(abundance.var, ".x", sep = "")]] != 0 | df12[[paste(abundance.var, ".y", sep = "")]] != 0)
     df12<-subset(df12, !is.na(df12[[paste(abundance.var, ".x", sep = "")]]) & !is.na(df12[[paste(abundance.var, ".y", sep = "")]]))
     
     # sort and apply turnover to all replicates
@@ -49,11 +53,10 @@ abundance_change <- function(df, time.var, species.var, abundance.var, replicate
     X <- split(df12, df12[[time.var]])
     
     
-    out <- lapply(X, FUN=abundchange, time.var, species.var, paste(abundance.var, ".x", sep = ""),paste(abundance.var, ".y", sep = "")) 
+    out <- lapply(X, FUN = abundchange, time.var, species.var, paste(abundance.var, ".x", sep = ""),paste(abundance.var, ".y", sep = "")) 
     output <- do.call("rbind", out)  
 
-  }
-  else{
+  } else {
     
     rankdf <- add_ranks_time(df,  time.var, species.var, abundance.var, replicate.var)
     
@@ -68,28 +71,28 @@ abundance_change <- function(df, time.var, species.var, abundance.var, replicate
     names(mytimes) = c(time.var, "dummytime")
     
     df1 <- merge(rankdf, mytimes)
-    names(df1)[1] <- 'time1'
-    names(df1)[[ncol(df1)]] <- time.var
+    names(df1)[1] = 'time1'
+    names(df1)[[ncol(df1)]] = time.var
     df1 <- subset(df1, !is.na(df1[[time.var]]))
     
     # merge: .x is for previous time point, .y for current time point, time.var corresponds to current (i.e., .y)
-    df12 <- merge(df1, df2,  by=c(species.var,replicate.var, time.var), all=T)
-    df12<-subset(df12, df12[[paste(abundance.var, ".x", sep = "")]]!=0|df12[[paste(abundance.var, ".y", sep = "")]]!=0)
+    df12 <- merge(df1, df2,  by = c(species.var,replicate.var, time.var), all = T)
+    df12<-subset(df12, df12[[paste(abundance.var, ".x", sep = "")]] !=0 | df12[[paste(abundance.var, ".y", sep = "")]] !=0)
     df12<-subset(df12, !is.na(df12[[paste(abundance.var, ".x", sep = "")]]) & !is.na(df12[[paste(abundance.var, ".y", sep = "")]]))
-    df12$splitvariable <- paste(df12[[replicate.var]], df12[[time.var]], sep="##") 
+    df12$splitvariable <- paste(df12[[replicate.var]], df12[[time.var]], sep = "##") 
     
     # sort and apply turnover to all replicates
     df12 <- df12[order(df12$splitvariable),]
     X <- split(df12, df12$splitvariable)
     
     
-    out <- lapply(X, FUN=abundchange, time.var, species.var, paste(abundance.var, ".x", sep = ""),paste(abundance.var, ".y", sep = "")) 
+    out <- lapply(X, FUN = abundchange, time.var, species.var, paste(abundance.var, ".x", sep = ""),paste(abundance.var, ".y", sep = "")) 
     ID <- unique(names(out))
     out <- mapply(function(x, y) "[<-"(x, "splitvariable", value = y) ,
                   out, ID, SIMPLIFY = FALSE)
     output <- do.call("rbind", out)  
     
-    outnames <- data.frame(do.call('rbind', strsplit(as.character(output$splitvariable),'##',fixed=TRUE)))
+    outnames <- data.frame(do.call('rbind', strsplit(as.character(output$splitvariable), '##', fixed = TRUE)))
     names(outnames) = c(replicate.var, time.var)
     outnames <- outnames[1]
     
@@ -97,6 +100,7 @@ abundance_change <- function(df, time.var, species.var, abundance.var, replicate
     output <- cbind(outnames, output)
     
   }
+  
   return(output)
 }
 
@@ -111,14 +115,15 @@ abundance_change <- function(df, time.var, species.var, abundance.var, replicate
 
 abundchange <- function(df, time.var, species.var, abundance.var1, abundance.var2){
 
-  time1.1<-unique(df$time1)
-  time1.2<-unique(df[[time.var]])
+  time1.1 <- unique(df$time1)
+  time1.2 <- unique(df[[time.var]])
   
-  df$abund_change <- df[[abundance.var1]]-df[[abundance.var2]]
-  df$time_pair<-paste(time1.1, time1.2, sep="-")
+  df$abund_change <- df[[abundance.var1]] - df[[abundance.var2]]
+  df$time_pair <- paste(time1.1, time1.2, sep = "-")
   
   df <- subset(df, select = c("time_pair", species.var, "abund_change"))
   
-  colnames(df)[1]<-paste(time.var, "pair", sep="_")
+  colnames(df)[1] <- paste(time.var, "pair", sep="_")
+  
   return(df)
 }
