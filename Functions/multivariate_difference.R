1b5c205c623d58d8be45d3132078dc3b8dcf09e8
Meghan Avolio <meghanavolio@Meghans-MB.local>
2018-01-22 19:51:38 -0500

    fixing problem with split varaible

--- Functions/multivariate_difference.R
+++ Functions/multivariate_difference.R
@@ -37,10 +37,10 @@ mult_diff <- function(df, species.var, abundance.var, replicate.var, treatment.v
   
   #transpose data
   df2<-subset(df, select = c(species.var, abundance.var, replicate.var, treatment.var))
-  df2$id <- paste(df2[[treatment.var]], df2[[replicate.var]], sep="_")
+  df2$id <- paste(df2[[treatment.var]], df2[[replicate.var]], sep="##")
   species<-codyn:::transpose_community(df2, 'id', species.var, abundance.var)
   species$id <- row.names(species)
-  speciesid <- do.call(rbind.data.frame, strsplit(species$id, split="_"))
+  speciesid <- do.call(rbind.data.frame, strsplit(species$id, split="##"))
   colnames(speciesid)[1] <- treatment.var
   colnames(speciesid)[2] <- replicate.var
   species2 <- cbind(speciesid, species)
